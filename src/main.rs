ba61fd0a42f1dd8657331aafdb40a23713f426d9
kirinokirino <kirinokirino2501@gmail.com>
2023-10-10 23:09:37 +0300

    t

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,57 @@
-use std::borrow::BorrowMut;
+use std::io::Write;
+use std::process::{Command, Stdio};
 
+const RECORD: bool = true;
 const WIDTH: usize = 640;
 const HEIGHT: usize = 480;
 
 fn main() {
-    let mut palette: Vec<_> = ["#df8c00", "#d7ac64", "#36241e", "#4a3d35", "#747769"].iter().map(hex_to_rgb).collect();
-    palette.extend([[0,0,0,0]].repeat(30));
+    let ffmpeg_command = "/usr/bin/ffmpeg";
+    let args = "-y -f rawvideo -vcodec rawvideo -s 640x480 -pix_fmt rgba -r 30 -i - -an -vcodec h264 -pix_fmt yuv420p -crf 15 /home/kirinokirino/Media/video.mp4".split(' ');
+    let mut ffmpeg = if RECORD {
+        Some(
+            Command::new(ffmpeg_command)
+                .args(args)
+                .stdin(Stdio::piped())
+                .spawn()
+                .expect("failed to execute process")
+                .stdin
+                .take()
+                .unwrap(),
+        )
+    } else {
+        None
+    };
+
+    let mut palette: Vec<_> = ["#df8c00", "#d7ac64", "#36241e", "#4a3d35", "#747769"]
+        .iter()
+        .map(hex_to_rgb)
+        .collect();
+    palette.extend([[0, 0, 0, 0]].repeat(30));
     let mut canvas = Canvas::new(palette);
+    canvas.random();
     loop {
-        canvas.random();
+        canvas.update();
         canvas.display();
-        std::thread::sleep(std::time::Duration::from_millis(50));
+        if RECORD {
+            ffmpeg.as_mut().map(|mut ffmpeg| {
+                ffmpeg.write_all(
+                    &canvas
+                        .buffer.as_slice()
+                        // .iter()
+                        // .enumerate()
+                        // .filter(|(i, v)| i % 4 != 3)
+                        // .map(|(i, v)| *v)
+                        // .collect::<Vec<u8>>(),
+                )
+            });
+        }
+        std::thread::sleep(std::time::Duration::from_millis(1));
     }
 }
 
 struct Canvas {
-    buffer: Vec<u8>,
+    pub buffer: Vec<u8>,
     palette: Vec<[u8; 4]>,
 }
 
@@ -27,6 +63,15 @@ impl Canvas {
         }
         buffer.fill(0);
         Self { buffer, palette }
+    }
+
+    pub fn update(&mut self) {
+        for pixel in self.buffer.as_mut_slice().chunks_exact_mut(4) {
+            pixel[0] = (pixel[0] as i32 + fastrand::i32(-2..3)).min(255).max(0) as u8;
+            pixel[1] = (pixel[1] as i32 + fastrand::i32(-2..3)).min(255).max(0) as u8;
+            pixel[2] = (pixel[2] as i32 + fastrand::i32(-2..3)).min(255).max(0) as u8;
+            pixel[3] = (pixel[3] as i32 + fastrand::i32(-2..3)).min(255).max(0) as u8;
+        }
     }
 
     pub fn display(&self) {
