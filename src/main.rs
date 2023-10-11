1e5f0f2ef5c879073e43350bb06f0febcb3be334
kirinokirino <kirinokirino2501@gmail.com>
2023-10-11 18:28:58 +0300

    glam and draw_line/point.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::io::Write;
 use std::process::{Command, Stdio};
 
-const RECORD: bool = true;
+const RECORD: bool = false;
 const WIDTH: usize = 640;
 const HEIGHT: usize = 480;
+
+use glam::Vec2;
 
 fn main() {
     let ffmpeg_command = "/usr/bin/ffmpeg";
@@ -29,7 +31,7 @@ fn main() {
         .collect();
     palette.extend([[0, 0, 0, 0]].repeat(30));
     let mut canvas = Canvas::new(palette);
-    canvas.random();
+    //canvas.random();
     loop {
         canvas.update();
         canvas.display();
@@ -53,6 +55,7 @@ fn main() {
 struct Canvas {
     pub buffer: Vec<u8>,
     palette: Vec<[u8; 4]>,
+    pen_color: [u8; 4],
 }
 
 impl Canvas {
@@ -62,10 +65,15 @@ impl Canvas {
             buffer.set_len(buffer.capacity());
         }
         buffer.fill(0);
-        Self { buffer, palette }
+        let pen_color = [255,255,255,255];
+        Self { buffer, palette, pen_color }
     }
 
     pub fn update(&mut self) {
+        self.buffer.fill(0);
+        self.draw_circle(Vec2::new(100.0, 100.0), 10.0);
+        self.draw_line(Vec2::new(20.0, 30.0), Vec2::new(150.0, 20.0));
+        return;
         for pixel in self.buffer.as_mut_slice().chunks_exact_mut(4) {
             pixel[0] = (pixel[0] as i32 + fastrand::i32(-2..3)).min(255).max(0) as u8;
             pixel[1] = (pixel[1] as i32 + fastrand::i32(-2..3)).min(255).max(0) as u8;
@@ -100,8 +108,42 @@ impl Canvas {
         }
     }
 
-    fn draw_circle(&mut self, x: usize, y: usize, radius: f32) {
-        todo!();
+    fn draw_line(&mut self, from: Vec2, to: Vec2) {
+        let delta = to - from;
+        let axis_biggest_distance = (delta.x).abs().max((delta.y).abs()) as usize;
+        let normalized = delta.normalize();
+        for step in 0..axis_biggest_distance {
+            let magnitude = step as f32;
+            let x = from.x + normalized.x * magnitude;
+            let y = from.y + normalized.y * magnitude;
+            self.draw_point(Vec2::new(x, y));
+        }
+    }
+
+    fn draw_circle(&mut self, pos: Vec2, radius: f32) {
+        let left_x = (pos.x - radius) as usize;
+        let right_x = (pos.x + radius) as usize;
+        let top_y = (pos.y - radius) as usize;
+        let bottom_y = (pos.y + radius) as usize;
+        for offset_x in left_x..right_x {
+            for offset_y in top_y..bottom_y {
+                if ((offset_x as f32 - pos.x as f32).powi(2) + (offset_y as f32 - pos.y as f32).powi(2)).sqrt() < radius {
+                    self.draw_point(Vec2::new(offset_x as f32, offset_y as f32));
+                }
+            }
+        }
+    }
+
+    fn draw_point(&mut self, pos: Vec2) {
+        let buffer_idx = self.idx(pos.x as usize, pos.y as usize);
+        self.buffer[buffer_idx] = self.pen_color[0];
+        self.buffer[buffer_idx + 1] = self.pen_color[1];
+        self.buffer[buffer_idx + 2] = self.pen_color[2];
+        self.buffer[buffer_idx + 3] = self.pen_color[3];
+    }
+
+    fn idx(&self, x: usize, y: usize) -> usize {
+        (x + y * WIDTH) * 4
     }
 }
 
